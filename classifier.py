609b1a8505465b44e23467840bfc663770095abb
Eugen Saraci <eugen.saraci@gmail.com>
2018-12-28 12:32:48 +0100

    better not lose progress

--- classifier.py
+++ classifier.py
@@ -2,16 +2,15 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-
-
-
 dataset = pd.read_csv("./dataset.csv")
 
-n_clusters = len(dataset.columns) - 1
+n_clusters = len(dataset.columns)-1
 data    = dataset.loc[:, "C1":"C{}".format(n_clusters)].values
 target  = dataset.loc[:, "action"].values
 
 X_train, X_test, y_train, y_test = train_test_split(data, target, random_state=1337)
 
-
+print(X_test)
 rfc = RandomForestClassifier(random_state=1337)
+rfc.fit(X_train, y_train)
+print(rfc.score(X_test, y_test))
--- main.py
+++ main.py
@@ -4,28 +4,22 @@ from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
 from dtw import dtw
 from fastdtw import fastdtw
 import matplotlib.pyplot as plt
+from multiprocessing import Pool
+from functools import partial
+import time
 
-df = pd.read_csv("./apps_total_plus_filtered.csv")
 
-def str_to_list(s):
+
+# CONSTS
+N_CLUSTERS      = 50
+N_FLOWS         = 10000
+PATH            = "./apps_total_plus_filtered.csv"
+
+def _str_to_list(s):
     return [int(sub.replace("[", "").replace("]", "")) for sub in s.split(",")]
 
 def _abs(x1, x2):
     return abs(x1-x2)
-
-# extracting useful data
-fb_data = df.loc[:5000,["action_start", "packets_length_total", "flow_length", "action"]].values
-
-# converting flows to actual lists
-flows = [str_to_list(row[1]) for row in fb_data]
-
-# condensed = upper triangular matrix
-n_flows = len(flows)
-condensed_dist = [dtw(flows[j], flows[k], _abs)[0]
-                      for j in range(n_flows)
-                          for k in range(j+1, n_flows)]
-
-Z = linkage(condensed_dist)
 
 """
 # dendrogram
@@ -40,62 +34,117 @@ dendrogram(
 )
 """
 
-n_clusters = 100
-res = fcluster(Z, n_clusters, criterion='maxclust')
+def load_raw_data(path=PATH):
+    df = pd.read_csv(path)
+    # extracting useful data
+    fb_data = df.loc[:N_FLOWS-1,["action_start", "packets_length_total", "flow_length", "action"]].values
+    # converting flows to actual lists
+    return [_str_to_list(row[1]) for row in fb_data], fb_data
+
+def cdm(flows, dist_func=_abs):
+    return [dtw(flows[i], flows[j], dist_func)[0]
+                          for i in range(N_FLOWS)
+                              for j in range(i+1, N_FLOWS)]
+
+def _triu_indexes():
+    return [(i, j) for i in range(N_FLOWS) for j in range(i+1, N_FLOWS)]
+
+def concurrent_cdm(flows, dist_func, pair):
+    return dtw(flows[pair[0]], flows[pair[1]], dist_func)[0]
+
+def clustering(cdm, linkage_metric="average"):
+    # cdm: precomputed condensed distance matrix
+    Z = linkage(cdm, method=linkage_metric)
+    return fcluster(Z, N_CLUSTERS, criterion='maxclust')
+
+def prepare_dataset(res, fb_data):
+
+    dataset             = []
+    features            = np.zeros(N_CLUSTERS)
+    prev_action_id      = 1383129102.11     # setting first value, => ugly but makes it easier
+    prev_action_label   = "open facebook"   # setting first value, => ugly but makes it easier
+    for idx, row in enumerate(fb_data):
+        # saving current action_{label, id}
+        cur_action_label    = row[3]
+        cur_action_id       = row[0]
+        # TODO: attenzione al caso base in cui action_{id, label} sono none <== not needed since hardcoded values set before loop
+        if prev_action_id != cur_action_id:
+            # if we skipped to the next action
+            # we have to save the current data and reset
+            # save data retrieved till now (action, features)
+            dataset.append((prev_action_label, features))
+            # reset features for next action
+            features = np.zeros(N_CLUSTERS)
+
+        # res[idx] = cluster associated with the idx-th flow
+        # features[res[idx]] number of elements in the res[idx]-th cluster for this action
+        # NB: clusters are numbered starting from 1, so we need res[idx] to be res[idx]-1  when used as index
+        features[res[idx]-1] += 1
+        prev_action_label = cur_action_label
+        prev_action_id = cur_action_id
+
+        if idx+1 == N_FLOWS:
+            # no more flow to analyze
+            # saving this one
+            dataset.append((prev_action_label, features))
+
+    return dataset
+
+def save_dataset(dataset):
+    # writing dataset to csv file
+    with open('./dataset.csv', 'w') as f:
+        # header row
+        # C1, C2,..., Cn, action
+        headers = ""
+        for i in range(N_CLUSTERS):
+            headers += "C{},".format(i+1)
+        headers += "action\n"
+        f.write(headers)
+
+        # data rows
+        # firs clusters, then action label
+        # row[0] = action label
+        # row[1] = list of cluters (features)
+        for row in dataset:
+            for c in row[1]:
+                f.write("{},".format(int(c)))
+            f.write("\"{}\"\n".format(row[0]))
 
 
-# preparing values 
-dataset             = []
-features            = np.zeros(n_clusters)
-prev_action_id      = 1383129102.11      # setting first value, => no need to handle None case
-prev_action_label   = "open facebook"   # setting first value, => no need to handle None case
+if __name__ == '__main__':
+    # F: flows loaded from dataset
+    # D: same cardinality as F but with more info needed later
+    # X: condensed distance matrix (upper triangular matrix)
+    # C: list with cluster assignments for each flow in D
+    
+    F, D = load_raw_data(path=PATH)
+    
+    start_time = time.time()
 
-for idx, row in enumerate(fb_data):
-    # saving current action_{label, id}
-    cur_action_label    = row[3]
-    cur_action_id       = row[0]
-    # TODO: attenzione al caso base in cui action_{id, label} sono none.
-    if prev_action_id != cur_action_id:
-        # if we skipped to the next action
-        # we have to save the current data and reset
-        # save data retrieved till now (action, features)
-        dataset.append((prev_action_label, features))
-        # reset features for next action
-        features = np.zeros(n_clusters)
+    CONCURRENT_EXEC = True
+    if CONCURRENT_EXEC:  
+        pool = Pool(4)
+        X_indexes = _triu_indexes()
+        caller = partial(concurrent_cdm, F, _abs)
+        X = pool.map(caller, X_indexes)
+        # np.savetxt("./async.txt", X)
+    
+    else:
+        X = cdm(flows=F, dist_func=_abs)
+        # np.savetxt("./sync.txt", X)
+
+    print("[----- cdm took {:.3f}s -----]".format(time.time() - start_time))
+
+    start_time = time.time()
+    C = clustering(cdm=X, linkage_metric="average")
+    print("[----- clustering + linkage=average took {:.3f}s -----]".format(time.time() - start_time))
 
     
-    # res[idx] = cluster associated with the idx-th flow
-    # features[res[idx]] number of elements in the res[idx]-th cluster for this action
-    # NB: clusters are numbered starting from 1, so we need res[idx] to be res[idx]-1  when used as index
-    features[res[idx]-1] += 1
-    prev_action_label = cur_action_label
-    prev_action_id = cur_action_id
-
-    if idx+1 == n_flows:
-        # no more flow to analyze
-        # saving this one
-        dataset.append((prev_action_label, features))
-
-
-# writing dataset to csv file
-with open('./dataset.csv', 'w') as f:
-    # header row
-    # C1, C2,..., Cn, action
-    headers = ""
-    for i in range(n_clusters):
-        headers += "C{},".format(i+1)
-    headers += "action\n"
-    f.write(headers)
-
-    # data rows
-    # firs clusters, then action label
-    # row[0] = action label
-    # row[1] = list of cluters (features)
-    for row in dataset:
-        for c in row[1]:
-            f.write("{},".format(int(c)))
-        f.write("\"{}\"\n".format(row[0]))
-
-
-
-
+    # DATASET CREATION RELATED
+    start_time = time.time()
+    dataset = prepare_dataset(res=C, fb_data=D)
+    print("[----- prepare_dataset took {:.3f}s -----]".format(time.time() - start_time))
+    
+    start_time = time.time()
+    save_dataset(dataset)
+    print("[----- save_dataset took {:.3f}s -----]".format(time.time() - start_time))
